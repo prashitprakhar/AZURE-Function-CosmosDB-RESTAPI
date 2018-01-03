ea2d7721ce8f4f30f014a5ba2ddafc7168641308
prashitprakhar <32621695+prashitprakhar@users.noreply.github.com>
2018-01-04 00:23:09 +0530

    Updating code for GET Specific Data call

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -1,20 +1,68 @@
-module.exports = function (context, req, documents) {
-    //context.log('JavaScript HTTP trigger function processed a request.');
-    const documentbind = context.bindings.documents;
-    
-    context.res = {
-            status: 200, /* Defaults to 200 */
-            body: {"name":documentbind[0].name}
+module.exports = function tableRouter(context, req) {
+    const res = context.res;
+    const id = context.bindingData.id;
+ 
+    switch (req.method) {
+        case 'GET':
+            if (id) {
+                getOneItem(req, res, id);
+            } else {
+                getAllItems(req, res);
+            }
+            break;
+ 
+        case 'POST':
+            insertItem(req, res);
+            break;
+ 
+        case 'PATCH':
+            patchItem(req, res, id);
+            break;
+ 
+        case 'PUT':
+            replaceItem(req, res, id);
+            break;
+ 
+        case 'DELETE':
+            deleteItem(req, res, id);
+            break;
+ 
+        default:
+            res.status(405).json({ error: "Operation not supported", message: `Method ${req.method} not supported`})
+    }
+}
+ 
+function getOneItem(req, res, id) {
+    if(id!='undefined' || id!='null'){
+    res.status(200).json({ id: id,
+                        "name": context.bindings.documents[0].name });
+    }
+    else{
+        res = {
+            status: 400,
+            body: "The Given ID is not valid. Please enter a valid 'ID'";
         };
-    
-    
-    //res.(documentbind[0].name);
-    
-    /*    for (var i = 0; i < documentbind.length; i++) {
-            var document = documentbind[i];
-            // operate on each document
-            context.log('First document Id : ', document[i].id);
-        } */
-
-    context.done();
-};
+    }
+}
+ 
+function getAllItems(req, res) {
+    res.status(200).json({ query: req.query, message: "getAll" });
+}
+ 
+function insertItem(req, res) {
+    res.status(200).json({ body: req.body, message: "insert"});
+}
+ 
+function patchItem(req, res, id) {
+    res.status(405).json({ error: "Not Supported", message: "PATCH operations are not supported" });
+}
+ 
+function replaceItem(req, res, id) {
+    res.status(200).json({ body: req.body, id: id, message: "replace" });
+}
+ 
+function deleteItem(req, res, id) {
+    res.status(200).json({ id: id, message: "delete" });
+}
+ 
+module.exports = tableRouter;
