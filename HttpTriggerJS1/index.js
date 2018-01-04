d1bbb1ce91b903ece4fcb4f45fb3d5f6a342237e
prashitprakhar <32621695+prashitprakhar@users.noreply.github.com>
2018-01-04 16:49:48 +0530

    Updating code for GET and POST

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -1,18 +1,18 @@
-function tableRouter(context, req) {
-    const res = context.res;
-    const id = context.bindingData.id;
+function restAPIRedirect(context, req) {
+   // const res = context.res;
+   // const id = context.bindingData.id;
  
     switch (req.method) {
         case 'GET':
-            if (id) {
-                getOneItem(context, req, res, id);
+            if (context.bindingData.id) {
+                getOneItem(context, req, context.res, context.bindingData.id);
             } else {
-                getAllItems(context, req, res);
+                getAllItems(context, req, context.res);
             }
             break;
  
         case 'POST':
-            insertItem(context, req, res);
+            insertItem(context, req, context.res);
             break;
  
         case 'PATCH':
@@ -20,7 +20,7 @@ function tableRouter(context, req) {
             break;
  
         case 'PUT':
-            replaceItem(req, res, id);
+            replaceItem(req);
             break;
  
         case 'DELETE':
@@ -43,35 +43,39 @@ function getOneItem(context, req, res, id) {
             "body": "The Given ID is not valid. Please enter a valid 'ID'"
         });
     }
-};
+    context.done();
+}
  
 function getAllItems(context, req, res) {
-        context.log(context.bindings.alldocuments[0].name);
-    //res.status(200).json({"AllInputs" : context.bindings.alldocuments });
-};
+
+    res.status(200).json({"AllInputs" : context.bindings.alldocuments });
+}
  
 function insertItem(context, req, res) {
-    //id: context.bindings.myQueueItem.name + "-" + context.bindings.myQueueItem.employeeId
+    
     context.bindings.employeeDocument = JSON.stringify({ 
-        id: context.bindings.myQueueItem.id,
-        name: context.bindings.myQueueItem.name
+        id: context.bindings.req.body.id,
+        name: context.bindings.req.body.name,
+        age: context.bindings.req.body.age
       });
 
-      context.done();
-
-    //res.status(200).json({ body: req.body, message: "insert"});
+        context.done();
 }
  
 function patchItem(req, res, id) {
-    res.status(405).json({ error: "Not Supported", message: "PATCH operations are not supported" });
+    //..
 }
  
-function replaceItem(req, res, id) {
-    res.status(200).json({ body: req.body, id: id, message: "replace" });
+function replaceItem(context, req, res) {
+
+    context.bindings.inputDocumentOut = context.bindings.inputDocumentIn;
+    //context.bindings.inputDocumentOut.text = "This was updated!";
+    context.done();
+    //res.status(200).json({ body: req.body, id: id, message: "replace" });
 }
  
 function deleteItem(req, res, id) {
     res.status(200).json({ id: id, message: "delete" });
 }
  
-module.exports = tableRouter;
+module.exports = restAPIRedirect;
